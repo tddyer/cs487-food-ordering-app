85083d3a3be24dee52aadaadb73469e9228fd92d
Tanner <tdyer@hawk.iit.edu>
2020-11-27 11:30:44 -0600

    created account login/guest login

--- app/src/main/java/com/example/orderquik/LoginActivity.java
+++ app/src/main/java/com/example/orderquik/LoginActivity.java
@@ -1,8 +1,20 @@
 package com.example.orderquik;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.io.Serializable;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +22,72 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void login(View v) {
+        switch (v.getId()) {
+            case R.id.userLogin: // launch main activity with login_id as entered email and password
+                // account login
+                userLogin();
+                break;
+            case R.id.guestLogin: // launch main activity with login_id as "guest" and no password
+                guestLogin();
+                break;
+            case R.id.staffLogin: // launch staff portal with valid staff credentials
+                // staff login
+                break;
+            default:
+                // do nothing
+                break;
+        }
+    }
+
+    public void guestLogin() {
+        // navigate to main activity with guest login info
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("LOGIN_ID", "guest");
+        intent.putExtra("LOGIN_PASSWORD", "");
+        startActivity(intent);
+    }
+
+    public void userLogin() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        @SuppressLint("InflateParams")
+        final View view = inflater.inflate(R.layout.user_login_layout, null);
+
+        builder.setView(view);
+        builder.setTitle("User Login");
+
+        builder.setPositiveButton("Login", (dialog, id) -> {
+            EditText em = findViewById(R.id.textEmail);
+            EditText password = findViewById(R.id.textPassword);
+
+            String email;
+            String pwrd;
+
+            try {
+                email = String.valueOf(em.getText());
+                pwrd = String.valueOf(password.getText());
+
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("LOGIN_ID", email);
+                intent.putExtra("LOGIN_PASSWORD", pwrd);
+                startActivity(intent);
+
+            } catch (NullPointerException npe) {
+                userLogin(); // could re-display with some error message upon invalid login
+            }
+
+        });
+
+        builder.setNegativeButton("Cancel", (dialog, id) -> {
+            // do nothing
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
