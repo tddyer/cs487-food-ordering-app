f192b723d17b19e76e06a53df94356465dd32d49
JuuDyung <41817239+JuuDyung@users.noreply.github.com>
2020-11-29 23:40:19 +0900

    Update Guest.java

--- app/src/main/java/com/example/orderquik/Guest.java
+++ app/src/main/java/com/example/orderquik/Guest.java
@@ -1,8 +1,9 @@
 package com.example.orderquik;
 
+import java.io.Serializable;
 import java.util.Random;
 
-public class Guest extends User {
+public class Guest extends User implements Serializable {
 
     // if a database was in use, this id would be a unique primary key for current active guests in the system
     private int guestId;
